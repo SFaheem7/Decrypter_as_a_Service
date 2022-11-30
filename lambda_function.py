9413b97219504250779d0cd3e57841f960fb53a3
SFaheem7 <Shahzeb96@hotmail.com>
2022-11-29 22:18:00 -0800

    Updated functionality and Swagger

---
+++ lambda_function.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import re
+s3_client = boto3.client('s3')
+S3_BUCKET = "hashtablesfk7"
+object_key = "data.csv"  
+
+
+def lambda_handler(event, context):
+	try:	
+		myHash = event['pathParameters']['shaHash']
+		s3_response = s3_client.select_object_content(Bucket='hashtablesfk7', Key='data.csv', ExpressionType='SQL', Expression=f"SELECT * FROM s3object s WHERE s.shaHash = '{myHash}'", InputSerialization = {'CSV': {"FileHeaderInfo": "Use"}, 'CompressionType': 'NONE'}, OutputSerialization = {'CSV': {}})
+		for event in s3_response["Payload"]:
+			if 'Records' in event:
+				records = event['Records']['Payload'].decode('utf-8')
+				length = len(records)
+				return{
+		    		'statusCode': 200,
+		    		'body': json.dumps(records[0:length-2].replace(',',' : '))
+		    		}
+			else:
+				return{
+					'statusCode': 404,
+					'body': json.dumps('Hash not found!')
+		        }
+	except:
+			return{
+				'statusCode': 404,
+				'body': json.dumps('Hash not found!')
+		        }
+	
