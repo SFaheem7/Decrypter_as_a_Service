8c40a2df26c77130afb77645c7c3e626abd7380a
SFaheem7 <Shahzeb96@hotmail.com>
2022-12-01 19:49:03 -0800

    Fixed JSON output

--- lambda_function.py
+++ lambda_function.py
@@ -9,14 +9,16 @@ object_key = "data.csv"
 def lambda_handler(event, context):
 	try:	
 		myHash = event['pathParameters']['shaHash']
-		s3_response = s3_client.select_object_content(Bucket='hashtablesfk7', Key='data.csv', ExpressionType='SQL', Expression=f"SELECT * FROM s3object s WHERE s.shaHash = '{myHash}'", InputSerialization = {'CSV': {"FileHeaderInfo": "Use"}, 'CompressionType': 'NONE'}, OutputSerialization = {'CSV': {}})
+		s3_response = s3_client.select_object_content(Bucket='hashtablesfk7', Key='data.csv', ExpressionType='SQL', Expression=f"SELECT s.password FROM s3object s WHERE s.shaHash = '{myHash}'", InputSerialization = {'CSV': {"FileHeaderInfo": "Use"}, 'CompressionType': 'NONE'}, OutputSerialization = {'CSV': {}})
 		for event in s3_response["Payload"]:
 			if 'Records' in event:
 				records = event['Records']['Payload'].decode('utf-8')
 				length = len(records)
+				passWord = records[0:length-2]
 				return{
 		    		'statusCode': 200,
-		    		'body': json.dumps(records[0:length-2].replace(',',' : '))
+		    		'body': 
+		    			json.dumps({myHash:passWord})
 		    		}
 			else:
 				return{
@@ -28,4 +30,3 @@ def lambda_handler(event, context):
 				'statusCode': 404,
 				'body': json.dumps('Hash not found!')
 		        }
-	
